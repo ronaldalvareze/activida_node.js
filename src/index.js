ac2fb91a7d846c25c0897059b863c728017a21a5
JSTATAN <jsgonzalez198@soy.sena.edu.co>
2023-08-18 21:08:02 -0500

    agragado los router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import express from "express"
 import {dirname, join} from 'path'
 import { fileURLToPath } from "url"
+import indexRoutes from './routes/index.js'
 
 const app = express()
 
 const _dirname = dirname(fileURLToPath(import.meta.url))
 console.log(join(_dirname, '/views'))
 app.set('views', join(_dirname, 'views'))
-
 app.set('view engine', 'ejs')
+app.use(indexRoutes)
 
-app.get('/',(req, res) => res.render('index'))
+
 
 app.listen(3000)
 console.log("server online", 3000)
