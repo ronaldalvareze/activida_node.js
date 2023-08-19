2766f40f37f53f4f80ef7258a87c5f2765675a3e
JSTATAN <jsgonzalez198@soy.sena.edu.co>
2023-08-18 19:43:08 -0500

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 
+
+
 const app = express()
+
+app.set('view engine' , 'ejs')
+
+app.get('/' ,(req, res) => res.send('hey funciona'))
 
 app.listen(3000)
 console.log("hey funciona" ,3000)
